6fee0c3f6b59384326438b124639e32b405a30cd
Provatsoft <provatsoft@gmail.com>
2019-07-26 12:35:34 +0600

    Test Null Pointer Exception

--- kotlin-native-pratices/hands-on/HelloKotlin/src/main.kt
+++ kotlin-native-pratices/hands-on/HelloKotlin/src/main.kt
@@ -1,5 +1,17 @@
 
 
 fun main(){
-    print("Hello World!")
+    try {
+        print("Hello World!")
+
+        var b:String?=null
+
+        val l = b?.length
+
+        print("Value $l")
+    }catch (e:Exception){
+        e.printStackTrace()
+    }
+
+
 }
